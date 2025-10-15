7e73f5697b1a723607a242a10de16fe907970e40
Aluno SENAI <Aluno@SN-353329.edu.fiesc.com.br>
2025-10-15 16:30:10 -0300

    codigo conexao db

---
+++ server.js
@@ -0,0 +1,43 @@
+import Fastify from 'fastify'
+import { Pool } from 'pg'
+
+const server = Fastify()
+const pool = new Pool({
+    host: 'dpg-d3ntiare5dus73bgj9o0-a',
+    port: '5432',
+    database: 'testedb_19tm',
+    user: 'teste_backend',
+    password: 'aFgEzIRRNTpuoXVgMf0UB5sUzIsg7GNZ',
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
+
+server.get('/criartabelausuario', async (request, reply) => {
+
+
+    try{
+
+        await pool.query(`CREATE TABLE USUARIO(
+                ID SERIAL PRIMARY KEY,
+                NOME VARCHAR(100) NOT NULL,
+                SENHA VARCHAR(100) NOT NULL,
+                EMAIL VARCHAR(100) NOT NULL UNIQUE
+            );`)
+
+        reply.status(200).send('Tabela criada com sucesso')
+    }
+    catch(e){
+        reply.status(500).send(`Erro ao criar tabela usuários: ${e}`)
+    }
+})
+
+
+
+
+
+server.listen({
+    port: 6969
+})
+
+
